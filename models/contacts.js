8825fc4e93bb669ca31a50934479bac77279c0cf
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-10-28 11:34:20 +0200

    getDone

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,32 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
+const path = require('path');
+// const {nanoid} = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve('./models/contacts.json');
 
-const getContactById = async (contactId) => {}
+// TODO: udokumentuj każdą funkcję
+const listContacts = async() => {
+  try {
+      const contactsTMP = await fs.readFile(contactsPath);
+      const contacts = await JSON.parse(contactsTMP);
+      return contacts 
+  }
+  catch(error) {
+      console.log(error)
+  }
+}
+
+const getContactById = async (contactId) => {
+  try {
+    const contactsTMP = await listContacts();
+    const contact = await contactsTMP.filter((elem)=>elem.id===contactId.toString());
+    return contact
+  } 
+  catch (error) {
+    console.log(error.message)
+    return false
+  }
+}
 
 const removeContact = async (contactId) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,29 @@
 const express = require('express')
-
 const router = express.Router()
+const contacts = require("../../models/contacts")
+
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const response = await contacts.listContacts()
+  res.status(200).json({ 
+    status: 200,
+    data: response });
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId} = req.params;
+  const contactsTMP = await contacts.getContactById(contactId);
+  const isIdFound = await contactsTMP.find(contact => contact.id === contactId);
+
+  if (isIdFound) {
+    const response = await contacts.getContactById(contactId);
+    res.status(200).json({ 
+      status: 200,
+      data: response });
+  } else {
+    res.status(404).json({ message: 'Not found' })
+  }
+
 })
 
 router.post('/', async (req, res, next) => {
