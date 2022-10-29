3b6311568cecabac5bbf9942fa242a50916877fa
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-10-29 12:16:54 +0200

    fnDone

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
-// const {nanoid} = require("nanoid");
+const {nanoid} = require("nanoid");
 
 const contactsPath = path.resolve('./models/contacts.json');
 
@@ -28,11 +28,54 @@ const getContactById = async (contactId) => {
   }
 }
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contactsTMP = await listContacts();
+    const contacts = await contactsTMP.filter((elem)=>elem.id !== contactId);
+    await fs.writeFile(contactsPath,JSON.stringify(contacts));
+    return contacts
 
-const addContact = async (body) => {}
+  } 
+  catch (error) {
+    console.log(error.message)
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (name, email, phone) => {
+  try {
+    const contactsTMP = await listContacts();
+    const newContact = {
+      id:nanoid(),
+      name: name,
+      email: email,
+      phone: phone,
+    }
+
+    const contacts = [...contactsTMP, newContact]
+    await fs.writeFile(contactsPath,JSON.stringify(contacts));
+    return contacts
+  } 
+  catch (error) {
+    console.log(error.message)
+  }
+}
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contactsTMP = await listContacts();
+    const upDatedContacts = await contactsTMP.map((elem)=>elem.id===contactId.toString() ? {...elem,...body}: elem);
+    console.log(upDatedContacts);
+    // const contactsWithoutDeletedContact = await contactsTMP.filter((elem)=>elem.id !== contactId);
+    // const newContacts = [...contactsWithoutDeletedContact, uptatedContact]
+    await fs.writeFile(contactsPath,JSON.stringify(upDatedContacts));
+    const uptatedContact = await upDatedContacts.filter((elem)=>elem.id===contactId.toString());
+    return uptatedContact
+  } 
+  catch (error) {
+    console.log(error.message)
+    return false
+  }
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+// const { nanoid } = require('nanoid')
 const router = express.Router()
 const contacts = require("../../models/contacts")
 
@@ -27,15 +28,58 @@ router.get('/:contactId', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const {name, email, phone} = req.body;
+  const canSave = [name, email, phone].every(Boolean);
+  console.log(canSave);
+  if (canSave) {
+    const response = await contacts.addContact(name, email, phone)
+    res.status(201).json({
+      status:201,
+      data:response
+    })
+  } else {
+    res.status(400).json({ message: 'missing required field' })
+  }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId} = req.params;
+  const contactsTMP = await contacts.getContactById(contactId);
+  const isIdFound = await contactsTMP.find(contact => contact.id === contactId);
+  const body = req.body;
+  const {name, email, phone} = body;
+  // console.log(body)
+  const canSave = [name,email,phone].some(Boolean);
+  if (isIdFound) {
+    if (canSave) {
+      const response = await contacts.updateContact(contactId, body);
+      res.status(200).json({ 
+        status: 200,
+        message: response});
+    } else {
+      res.status(400).json({ message: 'missing fields' })
+    }
+  } else {
+    res.status(404).json({ message: 'Not found' })
+  }
+})
+
+
+router.delete('/:contactId', async (req, res, next) => {
+  const {contactId} = req.params;
+  const contactsTMP = await contacts.getContactById(contactId);
+  const isIdFound = await contactsTMP.find(contact => contact.id === contactId);
+
+  if (isIdFound) {
+    await contacts.removeContact(contactId);
+    res.status(200).json({ 
+      status: 200,
+      message: 'contact deleted'});
+  } else {
+    res.status(404).json({ message: 'Not found' })
+  }
 })
 
 module.exports = router
