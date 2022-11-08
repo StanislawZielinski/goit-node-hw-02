3f80b312f9801059d9394dd77aed02c621caff60
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-11-08 12:06:20 +0100

    Update joi.js

--- utils/joi/joi.js
+++ utils/joi/joi.js
@@ -1,16 +1,27 @@
 const Joi = require('joi');
 
-const schema = Joi.object().keys({
+const schemaPost = Joi.object().keys({
+    name: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30).
+        required(),
+    email: Joi.string()
+        .email({ tlds: false }).
+        required(),
+    phone: Joi.number().integer().required()
+})
+
+const schemaPut = Joi.object().keys({
     name: Joi.string()
         .alphanum()
         .min(3)
         .max(30),
     email: Joi.string()
         .email({ tlds: false }),
-    phone: Joi.number().integer(),
-    favorite: Joi.boolean(),
-    })
+    phone: Joi.number().integer()
+});
 
 module.exports = {
-    schema,
+    schemaPost, schemaPut
 }
